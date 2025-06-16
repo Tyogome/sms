e2112d22a472083e577452ac1a33146f61229a0f
Jinbo-12 <ymg2465002@stu.o-hara.ac.jp>
2025-06-16 12:33:27 +0900

    16日分

---
+++ src/scoremanager/main/SubjectListAction.java
@@ -0,0 +1,19 @@
+package scoremanager.main;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import tool.Action;
+
+public class SubjectListAction extends Action {
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		
+		
+		
+
+		// JSPへフォワード
+		req.getRequestDispatcher("subject_list.jsp").forward(req, res);
+	}
+}
