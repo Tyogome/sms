d736f999a4d4f926002281801d1dd02a7c78d417
Jinbo-12 <ymg2465002@stu.o-hara.ac.jp>
2025-06-25 10:01:34 +0900

    6/25

--- src/dao/SubjectDao.java
+++ src/dao/SubjectDao.java
@@ -151,7 +151,7 @@ public class SubjectDao extends Dao {
 			} else {
 				// 学生が存在した場合
 				// プリペアードステートメントにUPDATE文をセット
-				statement = connection.prepareStatement("update subject set Name = ?, cd = ?, school_cd = ?");
+				statement = connection.prepareStatement("update subject set name = ? where cd = ? and school_cd = ?;");
 				// プリペアードステートメントに値をバインド
 				statement.setString(1, subject.getName());
 				statement.setString(2, subject.getCd());
