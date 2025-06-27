0476394a03f1ca9fb0b4edd0bf5b2b613cd33ce1
Jinbo-12 <ymg2465002@stu.o-hara.ac.jp>
2025-06-27 10:54:01 +0900

    6/27

--- src/dao/TestListSubjectDao.java
+++ src/dao/TestListSubjectDao.java
@@ -5,7 +5,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import bean.Subject;
 import bean.TestListSubject;
@@ -29,6 +31,12 @@ public class TestListSubjectDao extends Dao {
 				testlistsubject.setStudentNo(rSet.getString("studentNo"));
 				testlistsubject.setStudentName(rSet.getString("studentName"));
 				testlistsubject.setClassNum(rSet.getString("classNum"));
+				//testlistsubject.setPoints(rSet.getMap<Integer,Integer>("points"));
+				Map<Integer, Integer> points = new HashMap<>();
+				points.put(1, rSet.getInt("no1")); // 1回目のテストの点数
+				points.put(2, rSet.getInt("no2")); // 2回目のテストの点数
+
+				testlistsubject.setPoints(points);
 				// リストに追加
 				list.add(testlistsubject);
 			}
@@ -54,13 +62,14 @@ public class TestListSubjectDao extends Dao {
 		/* 科目の成績一覧 */
 		try {
 			// プリペアードステートメントにSQL文をセット
-			statement = connection.prepareStatement("select student.ent_year, student.class_num, student.no as student_no, student.name,  no1.subject_cd, no1.point as no1, no2.point as no2 from studentleft join(select test.student_no, test.subject_cd, test.no as no1, test.point from test where no = ?) as no1 on student.no = no1.student_noleft join(select test.student_no, test.subject_cd, test.no as no2, test.point from test where no = ?)as no2 on student.no = no2.student_no and no1.subject_cd = no2.subject_cdwhere student.ent_year = ? and student.class_num = ? and  no1.subject_cd = ?;");
+			statement = connection.prepareStatement("select student.ent_year, student.class_num, student.no as student_no, student.name,  no1.subject_cd, no1.point as no1, no2.point as no2 from student left join(select test.student_no, test.subject_cd, test.no as no1, test.point from test where no = ?) as no1 on student.no = no1.student_no left join(select test.student_no, test.subject_cd, test.no as no2, test.point from test where no = ?)as no2 on student.no = no2.student_no and no1.subject_cd = no2.subject_cd where student.ent_year = ? and student.class_num = ? and  no1.subject_cd = ?;");
 			// プリペアードステートメントに学校コードをバインド
 			statement.setString(1, subject.getCd());
 			// プリペアードステートメントを実行
 			rSet = statement.executeQuery();
 			// リストへの格納処理を実行
 			list = postFilter(rSet);
+
 
 		} catch (Exception e) {
 			throw e;
@@ -87,14 +96,3 @@ public class TestListSubjectDao extends Dao {
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
